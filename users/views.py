9a9a4c484ad988df108e6ba6c2b520026e29c749
Nathaniel Mtundu <mtundunathaniel@gmail.com>
2025-11-08 08:13:34 +0300

    First commit

---
+++ users/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
+
+
+# Create your views here.
+def register_view(request):
+    if request.method=='POST':
+        form=UserCreationForm(request.POST)
+        if form.is_valid():
+            login(request, form.save())
+            return redirect("posts:list")
+    else:
+        form=UserCreationForm()
+    return render(request, "users/register.html", {"form": form})
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            login(request, form.get_user())
+            next_url = request.POST.get('next')
+            if next_url:
+                return redirect(next_url)
+            else:
+                return redirect("posts:new-post")
+            
+    else:
+        form = AuthenticationForm()
+    return render(request, "users/login.html", {"form": form})
+def logout_view(request):
+    if request.method=="POST":
+        logout(request)
+        return redirect("posts:list")
